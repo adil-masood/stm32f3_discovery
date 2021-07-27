3bae748c78da0aef1a2829281d8ddd4802b0c3a9
adil-masood <adil.root@outlook.com>
2021-07-27 18:21:06 +0500

    PWM_TIM3 with multiple cannels

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -88,17 +88,18 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM3_Init();
   MX_TIM1_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 	HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
 	// PC6 with 20% duty
 	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_1);
 	// PC7 with 40% duty
-	//HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_2);
 	// PC8 with 60% duty
-//	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_3);
+	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_3);
 	// PC9 with 80% duty
-//	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_4);
-	TIM3->CCR1=2;
+	HAL_TIM_PWM_Start(&htim3,TIM_CHANNEL_4);
+	TIM3->CCR1=35768;
 	//TIM3->CCR2=39321;
 //	TIM3->CCR3=26214;
 //	TIM3->CCR4=13107;
